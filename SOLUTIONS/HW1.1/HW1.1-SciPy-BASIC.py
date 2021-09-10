3e1d64012a6dce8578eed0527a4e5cd2511ff652
Your Name <jh2343@georgetown.edu>
2021-09-10 19:57:34 -0400

    SYNC-2021-09-10T19:57:34-04:00

--- SOLUTIONS/HW1.1/HW1.1-SciPy-BASIC.py
+++ SOLUTIONS/HW1.1/HW1.1-SciPy-BASIC.py
@@ -9,27 +9,23 @@ IPLOT=True
 INPUT_FILE='weight.json'
 FILE_TYPE="json"
 DATA_KEYS=['x','is_adult','y']
-
-#UNCOMMENT FOR VARIOUS MODELS
-# model_type="logistic"; NFIT=4; xcol=1; ycol=2;
-model_type="linear";   NFIT=2; xcol=1; ycol=2; 
-# model_type="logistic";   NFIT=4; xcol=2; ycol=0;
-
-#HYPER-PARAM
 OPT_ALGO='BFGS'
+
+#UNCOMMENT FOR VARIOUS MODEL CHOICES (ONE AT A TIME)
+model_type="logistic"; NFIT=4; xcol=1; ycol=2;
+# model_type="linear";   NFIT=2; xcol=1; ycol=2; 
+# model_type="logistic";   NFIT=4; xcol=2; ycol=0;
 
 #READ FILE
 with open(INPUT_FILE) as f:
 	my_input = json.load(f)  #read into dictionary
 
-#CONVERT INPUT INTO ONE LARGE MATRIX 
+#CONVERT INPUT INTO ONE LARGE MATRIX (SIMILAR TO PANDAS DF)
 X=[];
 for key in my_input.keys():
-	if(key in DATA_KEYS):
-		X.append(my_input[key])
+	if(key in DATA_KEYS): X.append(my_input[key])
 
 #MAKE ROWS=SAMPLE DIMENSION (TRANSPOSE)
-#SIMILAR TO A PD DATAFRAME
 X=np.transpose(np.array(X))
 
 #SELECT COLUMNS FOR TRAINING 
@@ -37,23 +33,21 @@ x=X[:,xcol];  y=X[:,ycol]
 
 #EXTRACT AGE<18
 if(model_type=="linear"):
-	max_age=18; y=y[x[:]<max_age]; x=x[x[:]<max_age]; 
+	y=y[x[:]<18]; x=x[x[:]<18]; 
 
 #COMPUTE BEFORE PARTITION AND SAVE FOR LATER
 XMEAN=np.mean(x); XSTD=np.std(x)
 YMEAN=np.mean(y); YSTD=np.std(y)
 
 #NORMALIZE
-x=(x-XMEAN)/XSTD; y=(y-YMEAN)/YSTD; 
+x=(x-XMEAN)/XSTD;  y=(y-YMEAN)/YSTD; 
 
 #PARTITION
 f_train=0.8; f_val=0.2
 rand_indices = np.random.permutation(x.shape[0])
 CUT1=int(f_train*x.shape[0]); 
 train_idx,  val_idx = rand_indices[:CUT1], rand_indices[CUT1:]
-
-xt=x[train_idx]; yt=y[train_idx]
-xv=x[val_idx];   yv=y[val_idx]
+xt=x[train_idx]; yt=y[train_idx]; xv=x[val_idx];   yv=y[val_idx]
 
 #MODEL
 def model(x,p):
@@ -61,10 +55,12 @@ def model(x,p):
 	if(model_type=="logistic"): return  p[0]+p[1]*(1.0/(1.0+np.exp(-(x-p[2])/(p[3]+0.00001))))
 
 #SAVE HISTORY FOR PLOTTING AT THE END
-iterations=[]; loss_train=[];  loss_val=[]
-iteration=0
+iteration=0; iterations=[]; loss_train=[];  loss_val=[]
+
+#LOSS FUNCTION
 def loss(p):
 	global iterations,loss_train,loss_val,iteration
+
 	#TRAINING LOSS
 	yp=model(xt,p) #model predictions for given parameterization p
 	training_loss=(np.mean((yp-yt)**2.0))  #MSE
@@ -74,21 +70,20 @@ def loss(p):
 	validation_loss=(np.mean((yp-yv)**2.0))  #MSE
 
 	#WRITE TO SCREEN
-	if(iteration%25==0): print(iteration,training_loss,validation_loss) #,p)
+	if(iteration==0):    print("iteration	training_loss	validation_loss") 
+	if(iteration%25==0): print(iteration,"	",training_loss,"	",validation_loss) 
 	
 	#RECORD FOR PLOTING
 	loss_train.append(training_loss); loss_val.append(validation_loss)
-	iterations.append(iteration)
-
-	iteration+=1
+	iterations.append(iteration); iteration+=1
 
 	return training_loss
 
+#INITIAL GUESS
 po=np.random.uniform(0.5,1.,size=NFIT)
 
 #TRAIN MODEL USING SCIPY MINIMIZ 
-res = minimize(loss, po, method=OPT_ALGO, tol=1e-15)
-popt=res.x
+res = minimize(loss, po, method=OPT_ALGO, tol=1e-15);  popt=res.x
 print("OPTIMAL PARAM:",popt)
 
 #PREDICTIONS
@@ -100,7 +95,6 @@ def unnorm_x(x):
 	return XSTD*x+XMEAN  
 def unnorm_y(y): 
 	return YSTD*y+YMEAN 
-
 
 #FUNCTION PLOTS
 if(IPLOT):
@@ -118,8 +112,6 @@ if(IPLOT):
 	fig, ax = plt.subplots()
 	ax.plot(model(xt,popt), yt, 'o', label='Training set')
 	ax.plot(model(xv,popt), yv, 'o', label='Validation set')
-	# ax.plot(yt, yt, '-', label='y_pred=y_data')
-
 	plt.xlabel('y predicted', fontsize=18)
 	plt.ylabel('y data', fontsize=18)
 	plt.legend()
@@ -128,7 +120,6 @@ if(IPLOT):
 #MONITOR TRAINING AND VALIDATION LOSS  
 if(IPLOT):
 	fig, ax = plt.subplots()
-	#iterations,loss_train,loss_val
 	ax.plot(iterations, loss_train, 'o', label='Training loss')
 	ax.plot(iterations, loss_val, 'o', label='Validation loss')
 	plt.xlabel('optimizer iterations', fontsize=18)
